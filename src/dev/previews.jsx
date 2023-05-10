7cedc5ce81e5d4278b1c89fa7b46a1e28566f47d
djonnybravo <djonnybravo29@gmail.com>
2023-05-10 21:05:04 +0300

    create Counter Component

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import App from "../App";
+import Counter from "../Component/Counter";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/Counter">
+                <Counter/>
             </ComponentPreview>
         </Previews>
     )
