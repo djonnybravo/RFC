b4612af63ce3b39cc888903d08b228e7e9b12b71
djonnybravo <djonnybravo29@gmail.com>
2023-05-11 15:33:48 +0300

    add useState for posts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import PostItem from "./Component/PostItem";
 
 function App() {
 
-    let [value, setValue] = useState("Текст в инпуте")
+    let [posts, setPosts] = useState([
+        {id:1, title: 'Javascript', body: 'Description'},
+        {id:2, title: 'Javascript 2', body: 'Description'},
+        {id:3, title: 'Javascript 3', body: 'Description'}
+    ])
 
 
     return (
         <div className="App">
-            <PostItem post = {{id:1, title: 'Javascript', body: 'Description'}}/>
+            <h1 style={{textAlign: "center"}}>Список постов</h1>
+            {posts.map(post =>
+                <PostItem post={post} key={post.id}/>
+            )}
         </div>
     );
 }
