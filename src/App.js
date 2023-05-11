3bdaa90fc573ffa53f657168a11e3863a30c484f
djonnybravo <djonnybravo29@gmail.com>
2023-05-11 15:40:24 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import PostList from "./Component/PostList";
 
 function App() {
 
-    let [posts, setPosts] = useState([
-        {id:1, title: 'Javascript', body: 'Description'},
-        {id:2, title: 'Javascript 2', body: 'Description'},
-        {id:3, title: 'Javascript 3', body: 'Description'}
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'}
     ])
 
 
     return (
         <div className="App">
-            <PostList posts={posts}/>
+            <form>
+                <input type={"text"} placeholder={'Название поста'}/>
+                <input type={"text"} placeholder={'Описание поста'}/>
+                <button>Создать пост</button>
+            </form>
+            <PostList posts={posts} title={"Посты про JS"}/>
+
         </div>
     );
 }
