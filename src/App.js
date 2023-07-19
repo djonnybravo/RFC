bcb05260e49596e6d83a980c4f73972b2e41abbf
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-19 19:47:50 +0300

    feat: refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {Link, Navigate, Route, Routes} from "react-router-dom";
 import './styles/app.css'
-import About from "./pages/About";
-import Posts from "./pages/Posts";
 import Navbar from "./Component/UI/Navbar/Navbar";
-import Error from "./pages/Error";
+import AppRouter from "./Component/AppRouter";
 
 const App = () => {
     return (
         <div className={'app'}>
             <Navbar/>
-
-            <Routes>
-                <Route path={'/about'} element={<About/>}/>
-                <Route path={'/posts'} element={<Posts/>}/>
-                <Route path={'/error'} element={<Error/>}/>
-                <Route path={'*'} element={<Navigate to={'/error'}/>}/>
-            </Routes>
-
+            <AppRouter/>
         </div>
 
     );
