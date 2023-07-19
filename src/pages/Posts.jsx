bf9b4a589afd01df3a73291db64542c4fab76af5
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-19 17:43:08 +0300

    feat: add routing

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import '../styles/app.css'
 import {useFetching} from "../hooks/useFetching";
 import PostService from "../API/PostService";
 import MyButton from "../Component/UI/button/MyButton";
