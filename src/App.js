f7bfc950e1e898b9a117f389d1db16b387c8506d
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-19 17:43:06 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
+import './styles/app.css'
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path={'/about'} element={<About/>}/>
-            <Route path={'/posts'} element={<Posts/>}/>
-        </Routes>
+        <div className={'app'}>
+            <div className={'navbar'}>
+                <div className={'navbar__items'}>
+                    <Link to={'/posts'}>Posts</Link>
+                    <Link to={'/about'}> About</Link>
+                </div>
+            </div>
+
+            <Routes>
+
+                <Route path={'/about'} element={<About/>}/>
+                <Route path={'/posts'} element={<Posts/>}/>
+            </Routes>
+
+
+        </div>
+
     );
 };
 
