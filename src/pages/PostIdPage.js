debf5889e94700331fd82a458fbbdf3e5640e357
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-21 22:44:11 +0300

    feat: try dynamic routing

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 const PostIdPage = () => {
+    const params = useParams()
     return (
         <div>
-            <h1></h1>
+            <h1>Вы открыли пост с ID #{params.id}</h1>
         </div>
     );
 };
