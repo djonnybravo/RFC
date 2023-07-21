7142c0056fcecdcb22b5fb7c2678bfc20770c2ef
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-21 22:59:28 +0300

    feat: try dynamic routing

--- src/pages/PostIdPage.js
+++ src/pages/PostIdPage.js
@@ -2,26 +2,33 @@ import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {useFetching} from "../hooks/useFetching";
 import PostService from "../API/PostService";
+import Loader from "../Component/UI/Loader/Loader";
 
 const PostIdPage = () => {
     const params = useParams()
-    const [post, setPost] = useState(null)
-    const [fetchPostById, isLoading, error ] = useFetching(async (id) => {
-        const response =  await PostService.getById(id)
+
+
+    const [post, setPost] = useState({})
+    const [fetchPostById, isLoading, error ] = useFetching(async () => {
+        const response =  await PostService.getById(params.id)
+        console.log(response.data)
         setPost(response.data)
     })
 
 
     useEffect( () => {
-        fetchPostById(params.id)
+        fetchPostById()
     }, [])
 
     return (
         <div>
             <h1>Вы открыли пост с ID #{params.id}</h1>
-            {is}
-            <div>{post.id}. {post.title}</div>
-            <div>{post.body}</div>
+            {
+                isLoading
+                    ? <Loader/>
+                    : <div>{post.id}. {post.title}</div>
+            }
+
         </div>
     );
 };
